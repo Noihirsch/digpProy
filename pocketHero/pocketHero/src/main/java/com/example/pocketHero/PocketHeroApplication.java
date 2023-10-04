94ca6c9b8057a75061894dcd1be3e6d5934632f6
noihirsch <10dianagoncalves@gmail.com>
2023-10-04 13:18:52 +0200

    Agregar archivos de proyecto.

---
+++ pocketHero/pocketHero/src/main/java/com/example/pocketHero/PocketHeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocketHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketHeroApplication.class, args);
+	}
+
+}
---
+++ pocketHero/pocketHero/src/test/java/com/example/pocketHero/PocketHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocketHero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
