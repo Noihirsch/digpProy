8a537f567d2e4789233c9707422552dc6c83eaf1
noihirsch <10dianagoncalves@gmail.com>
2024-01-11 09:18:51 +0100

    redefined folder structures (creator)

--- pocketHero/pocketHero/src/main/java/com/example/pocketHero/domains/creator/Character.java
+++ pocketHero/pocketHero/src/main/java/com/example/pocketHero/domains/creator/Character.java
@@ -1,4 +1,4 @@
-package com.example.pocketHero.domains;
+package com.example.pocketHero.domains.creator;
 
 public class Character {
 
---
+++ pocketHero/pocketHero/src/main/java/com/example/pocketHero/domains/creator/Player.java
@@ -0,0 +1,5 @@
+package com.example.pocketHero.domains.creator;
+
+public class Player {
+    
+}
